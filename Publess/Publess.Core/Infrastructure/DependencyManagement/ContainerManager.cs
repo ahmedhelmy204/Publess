f48007bfb4e79059588d8af102e2c67abd25d25a
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-05-02 10:38:31 +0200

    Working with dependency injection

--- Publess/Publess.Core/Infrastructure/DependencyManagement/ContainerManager.cs
+++ Publess/Publess.Core/Infrastructure/DependencyManagement/ContainerManager.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
 
 namespace Publess.Core.Infrastructure.DependencyManagement
 {
     public class ContainerManager
     {
-
+        private readonly IUnityContainer _container;
     }
 }

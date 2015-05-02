c84c05eebaad13cfa76cb19109b728d761de33b4
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-05-02 10:09:05 +0200

    Add Publess.Core.Infrastructure.IEngine.cs interface

--- Publess/Publess.Core/Infrastructure/EngineContext.cs
+++ Publess/Publess.Core/Infrastructure/EngineContext.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Publess.Core.Infrastructure
 {
-    class EngineContext
+    /// <summary>
+    /// Provides access to the singleton instance of the Publess engine.
+    /// </summary>
+    public class EngineContext
     {
+        #region Methods
+
+
+
+        #endregion Methods
     }
 }

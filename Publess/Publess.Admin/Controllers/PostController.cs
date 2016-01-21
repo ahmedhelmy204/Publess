691f92da7c044fe15f1d17a1261ae4234e5823a3
helmy204 <hamadaelking204@hotmail.com>
2016-01-21 14:26:13 +0200

    Publess.Admin.Post  Controller added, Dependency Injection Working, Generic Repository Working.

---
+++ Publess/Publess.Admin/Controllers/PostController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Publess.Service.Posts;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Publess.Admin.Controllers
+{
+    public class PostController : Controller
+    {
+        private IPostService _postService;
+
+        public PostController(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        // GET: /Post/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Publess/Publess.Admin/Startup.cs
+++ Publess/Publess.Admin/Startup.cs
@@ -36,6 +36,8 @@ namespace Publess.Admin
             services.AddApplicationInsightsTelemetry(Configuration);
 
             services.AddMvc();
+
+            Bootstrapper.Initialise(services, Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
